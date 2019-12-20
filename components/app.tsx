5f9210787e81b0bde039158a27cd2aa129cff267
Dries Hooghe <dries@cheqroom.com>
2019-12-21 00:08:37 +0100

    setup typescript + tailwind

---
+++ components/app.tsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class App extends Component {
+	render() {
+		return <p>This is the website of Dries Hooghe</p>;
+	}
+}
+
+export default App
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "./style/style.css";
+import App from "../components/app";
+
+const Index = () => {
+  return <App/>;
+}
+export default Index;
